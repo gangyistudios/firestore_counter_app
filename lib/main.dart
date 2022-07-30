73ad93dda31ad790826f57a1bac09ff8079fee69
Nissan Silvia <200sxmeda@gmail.com>
2022-07-30 11:57:51 +1000

    Configure Firestore Local Emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -7,6 +8,13 @@ import 'app.dart';
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  /// Connect to Local Firestore Emulator
+  FirebaseFirestore.instance.settings = const Settings(
+    host: 'localhost:8080',
+    sslEnabled: false,
+    persistenceEnabled: false,
   );
   runApp(const MyApp());
 }
